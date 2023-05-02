b1dd12bc970c306e7b3e387e0e95ffd6bc1d7c36
Doug Beckwith <dougcbeckwith@gmail.com>
2023-05-02 14:59:25 -0700

    add react-router-dom and set up some routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,33 @@
-import { useEffect, useState } from "react";
-function App() {
-  const [courses, setCourses] = useState(null);
-  // call api to get all courses
+import { Route, Routes } from "react-router-dom";
 
-  useEffect(() => {
-    const getCourses = async () => {
-      const response = await fetch("http://localhost:5000/api/courses");
-      const { courses } = await response.json();
-      console.log(courses);
-      setCourses(courses);
-    };
-    getCourses();
-  }, []);
+import Courses from "./components/Courses";
+import CourseDetail from "./components/CourseDetail";
+import CreateCourse from "./components/CreateCourse";
+import Error from "./components/Error";
+import NotFound from "./components/NotFound";
+import Header from "./components/Header";
+import Forbidden from "./components/Forbidden";
+import UpdateCourse from "./components/UpdateCourse";
+import UserSignIn from "./components/UserSignIn";
+import UserSignOut from "./components/UserSignOut";
+import UserSignUp from "./components/UserSignUp";
+
+function App() {
   return (
     <div>
-      {courses ? (
-        <div>
-          <h1>Available Courses</h1>
-          <a href="">Test</a>
-          <ul>
-            {courses.map((course, index) => (
-              <li key={index}>{course.title}</li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <p>No courses Available</p>
-      )}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Courses />} />
+        <Route path="courses/create" element={<CreateCourse />} />
+        <Route path="courses/:id/update" element={<UpdateCourse />} />
+        <Route path="courses/:id" element={<CourseDetail />} />
+        <Route path="signin" element={<UserSignIn />} />
+        <Route path="signup" element={<UserSignUp />} />
+        <Route path="signout" element={<UserSignOut />} />
+        <Route path="error" element={<Error />} />
+        <Route path="forbidden" element={<Forbidden />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
