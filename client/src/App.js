0b26beead57f8eabfbaedd8c85fc7d16d4dc7eb4
Doug Beckwith <dougcbeckwith@gmail.com>
2023-05-03 15:25:55 -0700

    update routes

--- client/src/App.js
+++ client/src/App.js
@@ -19,8 +19,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Courses />} />
         <Route path="courses/create" element={<CreateCourse />} />
-        <Route path="courses/:id/update" element={<UpdateCourse />} />
-        <Route path="courses/:id" element={<CourseDetail />} />
+        <Route path="courses/:courseId/update" element={<UpdateCourse />} />
+        <Route path="courses/:courseId" element={<CourseDetail />} />
         <Route path="signin" element={<UserSignIn />} />
         <Route path="signup" element={<UserSignUp />} />
         <Route path="signout" element={<UserSignOut />} />
